50542c086cc41aa91b4cb6e935383901a37de538
Theo Lemaire <theo.lemaire@epfl.ch>
2018-08-22 02:39:41 +0200

    added plotSignals function to pltutils.

--- PySONIC/plt/pltutils.py
+++ PySONIC/plt/pltutils.py
@@ -2,7 +2,7 @@
 # @Author: Theo Lemaire
 # @Date:   2017-08-23 14:55:37
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2018-08-21 16:10:36
+# @Last Modified time: 2018-08-22 01:41:49
 
 ''' Plotting utilities '''
 
@@ -1233,3 +1233,56 @@ def plotTraces(fpath, keys, tbounds):
         item.set_fontsize(fs)
     # fig.tight_layout()
     return fig
+
+
+def plotSignals(t, signals, states=None, ax=None, onset=None, lbls=None, fs=10):
+    ''' Plot several signals on one graph.
+
+        :param t: time vector
+        :param signals: list of signal vectors
+        :param states (optional): stimulation state vector
+        :param ax (optional): handle to figure axis
+        :param onset (optional): onset to add to signals on graph
+        :param lbls (optional): list of legend labels
+        :param fs (optional): font size to use on graph
+        :return: figure handle (if no axis provided as input)
+    '''
+
+    # If no axis provided, create figure
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(8, 3))
+        argout = fig
+    else:
+        argout = None
+
+    # Set axis aspect
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    for item in ax.get_xticklabels() + ax.get_xticklabels():
+        item.set_fontsize(fs)
+
+    # Compute number of signals
+    nsignals = len(signals)
+
+    # Add onset of provided
+    if onset is not None:
+        t0, y0 = onset
+        t = np.hstack((np.array([t0, 0.]), t))
+        signals = np.hstack((np.ones((nsignals, 2)) * y0, signals))
+
+    # Plot signals
+    for i, var in enumerate(signals):
+        ax.plot(t, var, label=lbls[i] if lbls is not None else None, c='C{}'.format(i))
+
+    # Add stimulation patches if states provided
+    if states is not None:
+        npatches, tpatch_on, tpatch_off = getPatchesLoc(t, states)
+        for i in range(npatches):
+            ax.axvspan(tpatch_on[i], tpatch_off[i], edgecolor='none',
+                       facecolor='#8A8A8A', alpha=0.2)
+
+    # Add legend
+    if lbls is not None:
+        ax.legend(fontsize=fs, frameon=False)
+
+    return argout
