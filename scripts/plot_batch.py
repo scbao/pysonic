4c4f11074ce9523dde2e581e3cd122be7715cd38
Theo Lemaire <theo.lemaire@epfl.ch>
2018-09-24 21:08:01 +0200

    cleaned up plot_batch and plot_comp scripts

--- scripts/plot_batch.py
+++ scripts/plot_batch.py
@@ -4,11 +4,10 @@
 # @Date:   2017-03-20 12:19:55
 # @Email: theo.lemaire@epfl.ch
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2018-08-21 16:07:30
+# @Last Modified time: 2018-09-24 21:05:28
 
 """ Batch plot profiles of several specific output variables of NICE simulations. """
 
-import sys
 import logging
 
 from PySONIC.utils import logger, OpenFilesDialog, InputError
@@ -17,24 +16,26 @@ from PySONIC.plt import plotBatch
 # Set logging level
 logger.setLevel(logging.INFO)
 
-# Select data files
-pkl_filepaths, pkl_dir = OpenFilesDialog('pkl')
-if not pkl_filepaths:
-    logger.error('No input file')
-    sys.exit(1)
+defaults = dict(
+    V_m=['Vm'],
+    Q_m=['Qm']
+)
 
 
-yvars = {
-    'V_m': ['Vm'],
-    'i_{Na}\ kin.': ['m', 'h', 'm3h', 'n'],
-    'i_K\ kin.': ['n'],
-    'i_M\ kin.': ['p']
-    # 'i_{CaL}\ kin.': ['q', 'r', 'q2r']
-}
+def main():
 
-# Plot profiles
-try:
-    plotBatch(pkl_dir, pkl_filepaths, title=True, vars_dict=yvars)
-except InputError as err:
-    logger.error(err)
-    sys.exit(1)
+    # Select data files
+    pkl_filepaths, pkl_dir = OpenFilesDialog('pkl')
+    if not pkl_filepaths:
+        logger.error('No input file')
+        return
+
+    # Plot profiles
+    try:
+        plotBatch(pkl_dir, pkl_filepaths, title=True, vars_dict=defaults)
+    except InputError as err:
+        logger.error(err)
+
+
+if __name__ == '__main__':
+    main()
--- scripts/plot_comp.py
+++ scripts/plot_comp.py
@@ -4,11 +4,10 @@
 # @Date:   2017-02-13 12:41:26
 # @Email: theo.lemaire@epfl.ch
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2018-08-21 16:07:31
+# @Last Modified time: 2018-09-24 21:07:21
 
 """ Compare profiles of several specific output variables of NICE simulations. """
 
-import sys
 import logging
 
 from PySONIC.utils import logger, OpenFilesDialog, InputError
@@ -17,16 +16,24 @@ from PySONIC.plt import plotComp
 # Set logging level
 logger.setLevel(logging.INFO)
 
-# Select data files
-pkl_filepaths, _ = OpenFilesDialog('pkl')
-if not pkl_filepaths:
-    logger.error('No input file')
-    sys.exit(1)
-nfiles = len(pkl_filepaths)
 
-# Comparative plot
-try:
-    plotComp('Qm', pkl_filepaths)
-except InputError as err:
-    logger.error(err)
-    sys.exit(1)
+default = 'Qm'
+
+
+def main():
+
+    # Select data files
+    pkl_filepaths, _ = OpenFilesDialog('pkl')
+    if not pkl_filepaths:
+        logger.error('No input file')
+        return
+
+    # Comparative plot
+    try:
+        plotComp(default, pkl_filepaths)
+    except InputError as err:
+        logger.error(err)
+
+
+if __name__ == '__main__':
+    main()
