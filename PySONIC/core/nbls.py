corrected nbls.quasiSteadyStates to return DC-averaged Vmeff, and adapted nbls.findRheobaseAmps accordingly.

PySONIC/core/nbls.py
@@ -4,7 +4,7 @@
 # @Date:   2016-09-29 16:16:19
 # @Email: theo.lemaire@epfl.ch
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2019-04-05 17:12:27
+# @Last Modified time: 2019-04-05 21:01:36
 
 import os
 import inspect
@@ -828,12 +828,12 @@ class NeuronalBilayerSonophore(BilayerSonophore):
                         is_computed[i] = 1
 
         # Return reference inputs and output arrays
-        return amps, charges, np.squeeze(Vmeff_on), np.squeeze(QS_states)
+        return amps, charges, np.squeeze(Vmeff_avg), np.squeeze(QS_states)
 
 
     def findRheobaseAmps(self, DCs, Fdrive, Vthr):
         ''' Find the rheobase amplitudes (i.e. threshold acoustic amplitudes of infinite duration
-            that would result in excitation) of a specific neuron for various stimulation duty cycles.
+            that would result in excitation) of a specific neuron for various duty cycles.
 
             :param DCs: duty cycles vector (-)
             :param Fdrive: acoustic drive frequency (Hz)
@@ -849,30 +849,29 @@ class NeuronalBilayerSonophore(BilayerSonophore):
 
         if DCs.size == 1:
             QS_states = QS_states.reshape((*QS_states.shape, 1))
+            Vmeff = Vmeff.reshape((*Vmeff.shape, 1))
 
-        Athr = np.empty(DCs.size)
-        for i, DC in enumerate(DCs):
-            # Compute US-ON and US-OFF net membrane current from QSS variables
-            iNet_on = self.neuron.iNet(Vmeff, QS_states[:, :, i])
-            iNet_off = self.neuron.iNet(Vthr, QS_states[:, :, i])
+        # Compute 2D QSS charge variation array at Qthr
+        dQdt = -self.neuron.iNet(Vmeff, QS_states)
 
-            # Compute the pulse average net current along the amplitude space
-            iNet_avg = iNet_on * DC + iNet_off * (1 - DC)
+        # Find the threshold amplitude that cancels dQdt for each duty cycle
+        Arheobase = np.array([np.interp(0, dQdt[:, i], Aref, left=0., right=np.nan)
+                              for i in range(DCs.size)])
 
-            # Find the threshold amplitude that cancels the pulse average net current
-            Athr[i] = np.interp(0, -iNet_avg, Aref, left=0., right=np.nan)
-
-        inan = np.where(np.isnan(Athr))[0]
+        # Check if threshold amplitude is found for all DCs
+        inan = np.where(np.isnan(Arheobase))[0]
         if inan.size > 0:
-            if inan.size == Athr.size:
-                logger.error('No rheobase amplitudes within [%s - %sPa] for the provided duty cycles',
-                             *si_format((Aref.min(), Aref.max())))
+            if inan.size == Arheobase.size:
+                logger.error(
+                    'No rheobase amplitudes within [%s - %sPa] for the provided duty cycles',
+                    *si_format((Aref.min(), Aref.max())))
             else:
                 minDC = DCs[inan.max() + 1]
-                logger.warning('No rheobase amplitudes within [%s - %sPa] below %.1f%% duty cycle',
-                               *si_format((Aref.min(), Aref.max())), minDC * 1e2)
+                logger.warning(
+                    'No rheobase amplitudes within [%s - %sPa] below %.1f%% duty cycle',
+                    *si_format((Aref.min(), Aref.max())), minDC * 1e2)
 
-        return Athr, Aref
+        return Arheobase, Aref
 
 
     def computeEffVars(self, Fdrive, Adrive, Qm, fs=None, phi=np.pi):

PySONIC/plt/QSS.py
@@ -8,10 +8,11 @@ from matplotlib import cm, colors
 from ..postpro import getFixedPoints, getEqPoint1D
 from ..core import NeuronalBilayerSonophore
 from .pltutils import *
+from ..constants import TITRATION_T_OFFSET
 
 
 def plotVarDynamics(neuron, a, Fdrive, Adrive, charges, varname, varrange, fs=12):
-    ''' Plot the QSS-aproximated derivative of a specific variable as function of
+    ''' Plot the QSS-approximated derivative of a specific variable as function of
         the variable itself, as well as equilibrium values, for various membrane
         charge densities at a given acoustic amplitude.
 
@@ -242,12 +243,12 @@ def plotQSSVarVsAmp(neuron, a, Fdrive, varname, amps=None, DC=1., Qi=None,
     for item in cbarax.get_yticklabels():
         item.set_fontsize(fs)
 
-    fig.canvas.set_window_title('{}_QSS_{}_vs_amp_{}scale'.format(neuron.name, varname, zscale))
+    fig.canvas.set_window_title('{}_QSS_{}_vs_A{}'.format(neuron.name, varname, zscale))
 
     return fig
 
 
-def plotEqChargeVsAmp(neurons, a, Fdrive, amps=None, tstim=250e-3, toffset=50e-3, PRF=100.0,
+def plotEqChargeVsAmp(neurons, a, Fdrive, amps=None, tstim=250e-3, PRF=100.0,
                       DCs=[1.], Qi=None, fs=12, xscale='lin', titrate=False):
     ''' Plot the equilibrium membrane charge density as a function of acoustic amplitude,
         given an initial value of membrane charge density.
@@ -289,6 +290,7 @@ def plotEqChargeVsAmp(neurons, a, Fdrive, amps=None, tstim=250e-3, toffset=50e-3
         # Compute 3D QSS charge variation array
         dQdt = -neuron.iNet(Vmeff, QS_states)
 
+        # For each duty cycle
         for j, DC in enumerate(DCs):
             color = 'C{}'.format(icolor)
 
@@ -298,9 +300,11 @@ def plotEqChargeVsAmp(neurons, a, Fdrive, amps=None, tstim=250e-3, toffset=50e-3
             for k, Adrive in enumerate(Aref):
                 dQ_profile = dQdt[k, :, j]
                 if Qi[i] is None:
-                    Qzeros = getFixedPoints(Qref, dQ_profile).tolist()
+                    Qzeros = getFixedPoints(Qref, dQ_profile)
+                    if Qzeros is not None:
+                        Qzeros = Qzeros.tolist()
                 else:
-                    Qzeros = [getEqPoint1D(Qref, dQdt[k, :, j], Qi[i])]
+                    Qzeros = [getEqPoint1D(Qref, dQ_profile, Qi[i])]
                 Qplot += Qzeros
                 Aplot += [Adrive] * len(Qzeros)
             ax.plot(np.array(Aplot) * 1e-3, np.array(Qplot) * 1e5, '.', c=color,
@@ -308,7 +312,7 @@ def plotEqChargeVsAmp(neurons, a, Fdrive, amps=None, tstim=250e-3, toffset=50e-3
 
             # If specified, compute and plot the threshold excitation amplitude
             if titrate:
-                Athr = nbls.titrate(Fdrive, tstim, toffset, PRF=PRF, DC=DC,
+                Athr = nbls.titrate(Fdrive, tstim, TITRATION_T_OFFSET, PRF=PRF, DC=DC,
                                     Arange=(Aref.min(), Aref.max()))  # Pa
                 ax.axvline(Athr * 1e-3, c=color, linestyle='--')
 
@@ -318,6 +322,12 @@ def plotEqChargeVsAmp(neurons, a, Fdrive, amps=None, tstim=250e-3, toffset=50e-3
     ax.legend(frameon=False, fontsize=fs)
     fig.tight_layout()
 
-    fig.canvas.set_window_title('Qeq_QSS_vs_amp_{}scale'.format(xscale))
+    fig.canvas.set_window_title('QSS_{}_vs_{}A_{}_{}%DC{}'.format(
+        'Qeq' if Qi[0] is not None else 'SFPs',
+        xscale,
+        '_'.join([n.name for n in neurons]),
+        '_'.join(['{:.0f}'.format(DC * 1e2) for DC in DCs]),
+        '_with_thresholds' if titrate else ''
+    ))
 
     return fig

scripts/plot_QSS_IQ.py
@@ -2,7 +2,7 @@
 # @Author: Theo Lemaire
 # @Date:   2018-09-28 16:13:34
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2019-04-05 17:28:42
+# @Last Modified time: 2019-04-05 18:46:36
 
 ''' Phase-plane analysis of neuron behavior under quasi-steady state approximation. '''
 
@@ -25,11 +25,14 @@ def main():
     ap.add_argument('-n', '--neurons', type=str, nargs='+', default=None, help='Neuron types')
     ap.add_argument('-o', '--outputdir', type=str, default=None, help='Output directory')
     ap.add_argument('-c', '--cmap', type=str, default='viridis', help='Colormap name')
-    ap.add_argument('-v', '--verbose', default=False, action='store_true', help='Increase verbosity')
-    ap.add_argument('-s', '--save', default=False, action='store_true', help='Save output figures')
-    ap.add_argument('--titrate', default=False, action='store_true', help='Titrate excitation threshold')
-    ap.add_argument('--tstim', type=float, default=250., help='Stimulus duration for titration (ms)')
-    ap.add_argument('--toffset', type=float, default=50., help='Offset duration for titration (ms)')
+    ap.add_argument('-v', '--verbose', default=False, action='store_true',
+                    help='Increase verbosity')
+    ap.add_argument('-s', '--save', default=False, action='store_true',
+                    help='Save output figures')
+    ap.add_argument('--titrate', default=False, action='store_true',
+                    help='Titrate excitation threshold')
+    ap.add_argument('--tstim', type=float, default=500.,
+                    help='Stimulus duration for titration (ms)')
     ap.add_argument('--PRF', type=float, default=100.,
                     help='Pulse-repetition-frequency for titration (Hz)')
     ap.add_argument('--DC', type=float, nargs='+', default=None, help='Duty cycle (%)')
@@ -37,6 +40,8 @@ def main():
                     help='Initial membrane charge density for phase-plane analysis (nC/cm2)')
     ap.add_argument('--Ascale', type=str, default='log',
                     help='Scale type for acoustic amplitude ("lin" or "log")')
+    ap.add_argument('-p', '--plotdetails', default=False, action='store_true',
+                    help='Plot details')
 
     # Parse arguments
     args = ap.parse_args()
@@ -60,24 +65,25 @@ def main():
         'log': np.logspace(np.log10(Arange[0]), np.log10(Arange[1]), 100)
     }[args.Ascale] * 1e3  # Pa
     tstim = args.tstim * 1e-3  # s
-    toffset = args.toffset * 1e-3  # s
     PRF = args.PRF  # Hz
     DCs = [100.] if args.DC is None else args.DC  # %
     DCs = np.array(DCs) * 1e-2  # (-)
 
-    # Plot iNet vs Q for different amplitudes for each neuron
     figs = []
-    logger.info('plotting iNet QSS profiles')
-    for i, neuron in enumerate(neurons):
-        for DC in DCs:
-            figs.append(plotQSSVarVsAmp(
-                neuron, a, Fdrive, 'iNet', amps=amps, DC=DC, Qi=Qi[i], zscale=args.Ascale))
+
+    if args.plotdetails:
+        # Plot iNet vs Q for different amplitudes for each neuron
+        logger.info('plotting iNet QSS profiles')
+        for i, neuron in enumerate(neurons):
+            for DC in DCs:
+                figs.append(plotQSSVarVsAmp(
+                    neuron, a, Fdrive, 'iNet', amps=amps, DC=DC, Qi=Qi[i], zscale=args.Ascale))
 
     # Plot equilibrium charge as a function of amplitude for each neuron
     logger.info('plotting equilibrium charges')
     figs.append(
         plotEqChargeVsAmp(
-            neurons, a, Fdrive, amps=amps, tstim=tstim, toffset=toffset, PRF=PRF, DCs=DCs,
+            neurons, a, Fdrive, amps=amps, tstim=tstim, PRF=PRF, DCs=DCs,
             Qi=Qi, xscale=args.Ascale, titrate=args.titrate))
 
     if args.save: