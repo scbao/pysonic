a8ba89d095f528dd6dc89a8370540971c184b286
Theo Lemaire <theo.lemaire@epfl.ch>
2020-07-15 12:18:27 +0200

    implemented passiveNeuron wrapper function to generate PassiveNeuron class (avoiding cross-modification of instances)

--- PySONIC/neurons/pas.py
+++ PySONIC/neurons/pas.py
@@ -3,58 +3,74 @@
 # @Email: theo.lemaire@epfl.ch
 # @Date:   2020-07-07 16:56:34
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2020-07-10 12:46:14
+# @Last Modified time: 2020-07-15 12:17:20
 
 from ..core import PointNeuron, addSonicFeatures
 
 
-@addSonicFeatures
-class PassiveNeuron(PointNeuron):
-    ''' Generic point-neuron model with only a passive current. '''
+def passiveNeuron(Cm0, gLeak, ELeak):
 
-    states = {}
+    @addSonicFeatures
+    class PassiveNeuron(PointNeuron):
+        ''' Generic point-neuron model with only a passive current. '''
 
-    def __new__(cls, Cm0, gLeak, ELeak):
-        ''' Initialization.
+        states = {}
 
-            :param Cm0: membrane capacitance (F/m2)
-            :param gLeak: leakage conductance (S/m2)
-            :param ELeak: leakage revwersal potential (mV)
-        '''
-        cls.Cm0 = Cm0
-        cls.gLeak = gLeak
-        cls.ELeak = ELeak
-        return super(PassiveNeuron, cls).__new__(cls)
+        def __new__(cls, Cm0, gLeak, ELeak):
+            ''' Initialization.
 
-    @property
-    def name(self):
-        return f'pas_tau_{self.tau_pas * 1e3:.2e}ms'
+                :param Cm0: membrane capacitance (F/m2)
+                :param gLeak: leakage conductance (S/m2)
+                :param ELeak: leakage revwersal potential (mV)
+            '''
+            cls.Cm0 = Cm0
+            cls.gLeak = gLeak
+            cls.ELeak = ELeak
+            return super(PassiveNeuron, cls).__new__(cls)
 
-    @property
-    def Cm0(self):
-        return self._Cm0
+        def copy(self):
+            return self.__class__(self.Cm0, self.gLeak, self.ELeak)
 
-    @Cm0.setter
-    def Cm0(self, value):
-        self._Cm0 = value
+        def __repr__(self):
+            params = {
+                'Cm0': f'{self.Cm0 * 1e2:.1f} uF/cm2',
+                'gLeak': f'{self.gLeak:.1f} S/m2',
+                'ELeak': f'{self.ELeak:.1f} mV'
+            }
+            params_str = ', '.join([f'{k} = {v}' for k, v in params.items()])
+            return f'{self.__class__.__name__}({params_str})'
 
-    @property
-    def Vm0(self):
-        return self.ELeak
+        @property
+        def name(self):
+            return f'pas_tau_{self.tau_pas * 1e3:.2e}ms'
 
-    @classmethod
-    def derStates(cls):
-        return {}
+        @property
+        def Cm0(self):
+            return self._Cm0
 
-    @classmethod
-    def steadyStates(cls):
-        return {}
+        @Cm0.setter
+        def Cm0(self, value):
+            self._Cm0 = value
 
-    @classmethod
-    def iLeak(cls, Vm):
-        ''' non-specific leakage current '''
-        return cls.gLeak * (Vm - cls.ELeak)  # mA/m2
+        @property
+        def Vm0(self):
+            return self.ELeak
 
-    @classmethod
-    def currents(cls):
-        return {'iLeak': lambda Vm, _: cls.iLeak(Vm)}
+        @classmethod
+        def derStates(cls):
+            return {}
+
+        @classmethod
+        def steadyStates(cls):
+            return {}
+
+        @classmethod
+        def iLeak(cls, Vm):
+            ''' non-specific leakage current '''
+            return cls.gLeak * (Vm - cls.ELeak)  # mA/m2
+
+        @classmethod
+        def currents(cls):
+            return {'iLeak': lambda Vm, _: cls.iLeak(Vm)}
+
+    return PassiveNeuron(Cm0, gLeak, ELeak)
